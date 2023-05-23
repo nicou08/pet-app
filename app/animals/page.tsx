1f3922b9426a909ffa395d7ba765a706bfa5c1d8
nicou08 <nicolas.guasca16@gmail.com>
2023-05-22 23:18:53 -0600

    Added animal info to animals page

--- app/animals/page.tsx
+++ app/animals/page.tsx
@@ -1,32 +1,31 @@
 import AnimalComp from "@/components/AnimalComp";
 
 async function getAnimals() {
-  const res = await fetch("http://localhost:3000/api/animals");
+  const res = await fetch("http://localhost:3000/api/animals", {
+    cache: "no-store",
+  });
   return res.json();
 }
 
 
-interface anm {
-  id: string;
-  animal: string;
-  type: string;
-  tags: string[];
-  description: string;
-}
-
-export default async function Animals() { 
+export default async function Animals() {
   const animalData = await getAnimals();
-  console.log("animalData")
-  console.log(animalData);
-  console.log("afterANIMALDATAAAAAA")
-
+  console.log("animalData");
 
   return (
     <div>
       <div className="text-4xl">ANIMALSssssAs PAGEee </div>
       <ul>
-        {animalData.map((anmm : anm) => (
-          <li key={anmm.id}>This is {anmm.animal}</li>
+        {animalData.map((anmm: any) => (
+          <li key={anmm._id}>
+            <AnimalComp
+              pet={anmm.animal}
+              image={anmm.image}
+              type={anmm.type}
+              tags={anmm.tags}
+              description={anmm.description}
+            />
+          </li>
         ))}
       </ul>
     </div>
--- components/AnimalComp.tsx
+++ components/AnimalComp.tsx
@@ -1,7 +1,31 @@
-export default function AnimalComp() {
-    return (
-        <div>
-            <div className="text-4xl">THIS IS THE ANIMAL COMPONENT</div>
-        </div>
-    )
+import Image from "next/image";
+import greg from "../public/greg.jpg";
+
+interface props {
+  pet: string;
+  image: string;
+  type: string;
+  tags: string[];
+  description: string;
+}
+
+export default function AnimalComp(props: props) {
+  return (
+    <div>
+      <div className="text-4xl">THIS IS THE ANIMAL COMPONENT</div>
+      <div> {props.pet} </div>
+      <div> {props.type} </div>
+      <div> {props.tags} </div>
+      <div> {props.description} </div>
+      {props.pet === "Guinea pigs" ? (
+        <>
+            {console.log("this is greg")}
+          <img src="/greg.jpeg"  alt="greg image" height={100} width={100} ></img>
+          <p>THIS IS GREG</p>
+        </>
+      ) : (
+        <img src={props.image} alt="pet image" height={100} width={100} />
+      )}
+    </div>
+  );
 }
