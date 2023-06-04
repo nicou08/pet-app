e3344752a161b1df2d256bbf837ba680add400f8
nicou08 <nicolas.guasca16@gmail.com>
2023-06-04 00:38:02 -0600

    c

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,32 @@
 import HomeCard from "@/components/HomeCard";
 
-async function getAnimalPics() {
-  const res = await fetch("https://pet-app-theta.vercel.app/api/animals", {
-    next: { revalidate: 60 },
-  });
-  // const res = await fetch("http://localhost:3000/api/animals", {
-  //   next: { revalidate: 60 },
-  // });
-  return res.json();
-}
+// async function getAnimalPics() {
+//   const res = await fetch("https://pet-app-theta.vercel.app/api/animals", {
+//     next: { revalidate: 60 },
+//   });
+//   // const res = await fetch("http://localhost:3000/api/animals", {
+//   //   next: { revalidate: 60 },
+//   // });
+//   return res.json();
+// }
+
+// export default async function Home() {
+//   const animalPicData = await getAnimalPics();
+
+//   return (
+//     <div className="w-full overflow-hidden">
+//       <div className="font-dancingScript font-bold text-center text-4xl pt-16 pb-16">
+//         Choose a pet you are interested in learning of!
+//       </div>
+//       <div className="w-full h-99 relative overflow-hidden">
+//         {animalPicData.map((animal: any) => (
+//           <HomeCard animalData={animal} key={animal._id}/>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
 export default async function Home() {
-  const animalPicData = await getAnimalPics();
-
-  return (
-    <div className="w-full overflow-hidden">
-      <div className="font-dancingScript font-bold text-center text-4xl pt-16 pb-16">
-        Choose a pet you are interested in learning of!
-      </div>
-      <div className="w-full h-99 relative overflow-hidden">
-        {animalPicData.map((animal: any) => (
-          <HomeCard animalData={animal} key={animal._id}/>
-        ))}
-      </div>
-    </div>
-  );
+  return <div>Hello</div>
 }
