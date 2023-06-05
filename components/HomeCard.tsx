924ab1dc081880d3ba54f3d9619bd503f3fb71b6
nicou08 <nicolas.guasca16@gmail.com>
2023-06-05 04:06:33 -0600

    Update HomeCard.tsx

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -18,7 +18,6 @@ export default function HomeCard(props: props) {
             pathname: `/animals/${props.animalData.animal}`,
             query: {
               pet: props.animalData.animal,
-              pet2: props.animalData.animal,
               type: props.animalData.type,
               tags: props.animalData.tags,
               image: "/greg.jpeg",
@@ -45,7 +44,7 @@ export default function HomeCard(props: props) {
           href={{
             pathname: `/animals/${props.animalData.animal}`,
             query: {
-              animal: props.animalData.animal,
+              pet: props.animalData.animal,
               type: props.animalData.type,
               tags: props.animalData.tags,
               image: props.animalData.image,
