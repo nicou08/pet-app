7c2cbb050f98c1d5eb8be88cdb99efdf564f9687
nicou08 <nicolas.guasca16@gmail.com>
2023-07-12 19:26:46 -0600

    Fixed header
    Changed header animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HomeCard from "@/components/HomeCard";
 
 async function getAnimalPics() {
   const res = await fetch("https://pet-app-theta.vercel.app/api/animals");
+  //const res = await fetch("http://localhost:3000/api/animals");
   // const res = await fetch("https://pet-app-theta.vercel.app/api/animals", {
   //   next: { revalidate: 60 },
   // });
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import "@/styles/global.css";
 import Link from "next/link";
@@ -7,24 +7,32 @@ import { usePathname } from "next/navigation";
 export default function Header() {
   function IsActive(route: string) {
     if (route === usePathname()) {
-      return "font-andika font-bold mx-2 text-3xl ease-in duration-300";
+      return "my-custom-class on-page relative font-bold";
     } else {
-      return "font-andika mx-7 ease-in duration-300";
+      return "my-custom-class relative";
     }
   }
 
   return (
-    <header className="relative h-24 bg-gradient-to-r from-yellow-200 via-orange-400 to-red-600">
-      <nav className="text-center text-white text-2xl pt-6">
-        <Link href="/" className={IsActive("/")}>
-          Home
-        </Link>
-        <Link href="/animals" className={IsActive("/animals")}>
-          Animals
-        </Link>
-        <Link href="/about" className={IsActive("/about")}>
-          About
-        </Link>
+    <header className="relative h-24 bg-gradient-to-r from-yellow-200 via-orange-500 to-red-500">
+      <nav className=" text-white text-xl pt-2">
+        <ul className="flex justify-center font-andika pt-5">
+          <li className="mr-20">
+            <Link href="/" className={IsActive("/")}>
+              Home
+            </Link>
+          </li>
+          <li className="mr-20">
+            <Link href="/animals" className={IsActive("/animals")}>
+              Animals
+            </Link>
+          </li>
+          <li className="">
+            <Link href="/about" className={IsActive("/about")}>
+              About
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
