74731e3348a6e219ec2ee1e2abc6d4dc42b24a3c
nicou08 <nicolas.guasca16@gmail.com>
2023-07-27 21:35:04 -0600

    Added a responsive design and fixed animal pages

--- app/animals/[animal]/page.tsx
+++ app/animals/[animal]/page.tsx
@@ -10,10 +10,13 @@ export default function Animal({ searchParams }: props) {
     console.log("CLICKED");
     console.log("ASDFASDFsearchParams: ", searchParams);
   }
-  
+
   return (
-    <div className="flex justify-center items-center h-101" key={searchParams._id}>
-      <div className="w-7/12 h-auto flex">
+    <div
+      className="flex justify-center xl:items-center h-[calc(100vh-9.5rem)] overflow-y-scroll "
+      key={searchParams._id}
+    >
+      <div className="w-11/12 lg:w-2/3 xl:w-[1200px] xl:flex pb-16 pt-5 ">
         {searchParams.pet === "Guinea pigs" ||
         searchParams.pet === "Hamsters" ||
         searchParams.pet === "Lizzards" ||
@@ -22,24 +25,26 @@ export default function Animal({ searchParams }: props) {
         searchParams.pet === "Hedgehogs" ||
         searchParams.pet === "Chickens" ||
         searchParams.pet === "Dog" ? (
-          <div style={{ width: "3000px", height: "auto" }}>
+          <div>
             <Image
               src={searchParams.image}
               alt="pet image"
-              className="w-full h-auto"
+              className="w-full xl:w-[4000px] h-auto"
             />
+
             {searchParams.pet === "Guinea pigs" ? (
-              <div>Special Note: This is my 2 year old guinea pig. His name is Greg.</div>
+              <div>
+                Special Note: This is my 2 year old guinea pig. His name is
+                Greg.
+              </div>
             ) : null}
           </div>
         ) : (
-          <div style={{ width: "1400px", height: "auto" }}>
-            <Image
-              src={searchParams.image}
-              alt="pet image"
-              className="w-full h-auto"
-            />
-          </div>
+          <Image
+            src={searchParams.image}
+            alt="pet image"
+            className="w-full xl:w-[600px]"
+          />
         )}
 
         <div className="p-5 font-andika">
@@ -51,7 +56,9 @@ export default function Animal({ searchParams }: props) {
           <div className="font-bold">Tags</div>
           <div className="flex pb-2">
             {searchParams.tags.map((tag: any, index: any) => (
-              <div className="mr-2" key={index}>{tag}</div>
+              <div className="mr-2" key={index}>
+                {tag}
+              </div>
             ))}
           </div>
           <div className="font-bold">Description</div>
--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -8,61 +8,76 @@ interface props {
 
 export default function HomeCard(props: props) {
   return (
-    <div className="shadow-xl rounded-xl w-96 bg-white travel1 invisible cursor-pointer" key={props.key}>
-      {props.animalData.animal === "Guinea pigs" ? (
-        console.log("INSIDE MAPPPP HOMECARD PROPS.animalData.animal === Guinea pigs: ", props.animalData.animal),
-        <Link
-          href={{
-            pathname: `/animals/${props.animalData.animal}`,
-            query: {
-              pet: props.animalData.animal,
-              type: props.animalData.type,
-              tags: props.animalData.tags,
-              image: "/greg.jpeg",
-              description: props.animalData.description,
-            },
-          }}
-        >
-          <Image
-            src="/greg.jpeg"
-            alt="greg image"
-            style={{ width: "100%", height: "auto" }}
-            className="rounded-t-xl"
-          />
-          <div className="p-5 flex">
-            {props.animalData.tags.map((tag: any, index: any) => (
-              <div className="mr-2" key={index}>{tag}</div>
-            ))}
-            <div>pachon</div>
-          </div>
-        </Link>
-      ) : (
-        console.log("INSIDE MAPPPP HOMECARD PROPS.animalData.animal: ", props.animalData.animal),
-        <Link
-          href={{
-            pathname: `/animals/${props.animalData.animal}`,
-            query: {
-              pet: props.animalData.animal,
-              type: props.animalData.type,
-              tags: props.animalData.tags,
-              image: props.animalData.image,
-              description: props.animalData.description,
-            },
-          }}
-        >
-          <Image
-            src={props.animalData.image}
-            alt="pet image"
-            style={{ width: "100%", height: "auto" }}
-            className="rounded-t-xl"
-          />
-          <div className="p-5 flex">
-            {props.animalData.tags.map((tag: any, index: any) => (
-              <div className="mr-2" key={index}>{tag}</div>
-            ))}
-          </div>
-        </Link>
-      )}
+    <div
+      className="shadow-xl rounded-xl w-96 bg-white travel1 invisible cursor-pointer"
+      key={props.key}
+    >
+      {props.animalData.animal === "Guinea pigs"
+        ? (console.log(
+            "INSIDE MAPPPP HOMECARD PROPS.animalData.animal === Guinea pigs: ",
+            props.animalData.animal
+          ),
+          (
+            <Link
+              href={{
+                pathname: `/animals/${props.animalData.animal}`,
+                query: {
+                  pet: props.animalData.animal,
+                  type: props.animalData.type,
+                  tags: props.animalData.tags,
+                  image: "/greg.jpeg",
+                  description: props.animalData.description,
+                },
+              }}
+            >
+              <Image
+                src="/greg.jpeg"
+                alt="greg image"
+                style={{ width: "100%", height: "auto" }}
+                className="rounded-t-xl"
+              />
+              <div className="p-5 flex">
+                {props.animalData.tags.map((tag: any, index: any) => (
+                  <div className="mr-2" key={index}>
+                    {tag}
+                  </div>
+                ))}
+                <div>pachon</div>
+              </div>
+            </Link>
+          ))
+        : (console.log(
+            "INSIDE MAPPPP HOMECARD PROPS.animalData.animal: ",
+            props.animalData.animal
+          ),
+          (
+            <Link
+              href={{
+                pathname: `/animals/${props.animalData.animal}`,
+                query: {
+                  pet: props.animalData.animal,
+                  type: props.animalData.type,
+                  tags: props.animalData.tags,
+                  image: props.animalData.image,
+                  description: props.animalData.description,
+                },
+              }}
+            >
+              <Image
+                src={props.animalData.image}
+                alt="pet image"
+                style={{ width: "100%", height: "auto" }}
+                className="rounded-t-xl"
+              />
+              <div className="p-5 flex">
+                {props.animalData.tags.map((tag: any, index: any) => (
+                  <div className="mr-2" key={index}>
+                    {tag}
+                  </div>
+                ))}
+              </div>
+            </Link>
+          ))}
     </div>
   );
 }
